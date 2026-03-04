7c9296636d49c1e39886628acde4fd900fa20df6
Keenan <keenanjj13@gmail.com>
2026-03-04 12:47:02 -0700

    feat: load persisted messages on app startup

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -3,18 +3,27 @@ import { AppShell } from './components/layout/AppShell';
 import { useAgentStore } from './store/agentStore';
 import { useSecretsStore } from './store/secretsStore';
 import { useSettingsStore } from './store/settingsStore';
+import { useTicketStore } from './store/ticketStore';
+import { useConversationStore } from './store/conversationStore';
+import { useMessageStore } from './store/messageStore';
 
 function App() {
   const { startPolling, stopPolling, restoreAgents } = useAgentStore();
   const { loadToken } = useSecretsStore();
   const { loadSettings } = useSettingsStore();
+  const { loadFromDisk: loadTickets } = useTicketStore();
+  const { loadFromDisk: loadConversations } = useConversationStore();
+  const { loadFromDisk: loadMessages } = useMessageStore();
 
   useEffect(() => {
     restoreAgents().then(() => startPolling());
     loadToken();
     loadSettings();
+    loadTickets();
+    loadConversations();
+    loadMessages();
     return () => stopPolling();
-  }, [startPolling, stopPolling, loadToken, loadSettings, restoreAgents]);
+  }, [startPolling, stopPolling, loadToken, loadSettings, restoreAgents, loadTickets, loadConversations, loadMessages]);
 
   return <AppShell />;
 }
